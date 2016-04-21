ed261f2dec1ca53d6a084cf6e2e7b5197bd3f492
Telmo Dias <Telmo Dias>
2016-04-21 23:15:20 +0100

    Projecto Inicial

---
+++ app/src/main/java/com/td1026/gpsemergencia/BaseDados/BDbackup.java
@@ -0,0 +1,107 @@
+package com.td1026.gpsemergencia.BaseDados;
+
+
+import android.content.Context;
+import android.os.Environment;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.Dao;
+import com.td1026.gpsemergencia.Dados.OcurrenciaActual;
+import com.td1026.gpsemergencia.Dados.Posicao;
+import com.td1026.gpsemergencia.MetodosAuxiliares.Formatos;
+import com.td1026.gpsemergencia.MetodosAuxiliares.Logs;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Telmo on 31-03-2016.
+ */
+public class BDbackup {
+
+
+    public static void gravarLocalmente (Context t, OcurrenciaActual o) {
+        List<bd_Trajecto> tar = new ArrayList<>();
+        bd_Ocurrencia nova = new bd_Ocurrencia(o.getOcurrencia());
+        try {
+            bd_Ocurrencia_Helper todoOpenDatabaseHelper = OpenHelperManager.getHelper(t, bd_Ocurrencia_Helper.class);
+            Dao<bd_Ocurrencia, Long> todoDao = todoOpenDatabaseHelper.getDao();
+            todoDao.create(nova);
+
+
+            bd_Trajecto_Helper bd_trajecto_helper = OpenHelperManager.getHelper(t, bd_Trajecto_Helper.class);
+            Dao<bd_Trajecto, Long> todoDao2 = bd_trajecto_helper.getDao();
+            for (Posicao p:o.getPercurso()) {
+                bd_Trajecto nova2 = new bd_Trajecto(nova.getId(), p);
+                todoDao2.create(nova2);
+                tar.add(nova2);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        gravarFicheiro(nova,tar);
+    }
+    public static void gravarPosgres (Context t, OcurrenciaActual o) {
+
+
+    }
+    public static void gravarFicheiro (bd_Ocurrencia o, List<bd_Trajecto> t) {
+        try {
+            if (!Logs.isExternalStorageWritable() || !Logs.isExternalStorageReadable()) {
+                return;
+            }
+            File sdCard = Environment.getExternalStorageDirectory();
+            File dir = new File(sdCard.getAbsolutePath() + "/" + Logs.Diretoria);
+            dir.mkdirs();
+            File file = new File(dir,"oc_" + o.getId() + "_"+ Formatos.getDataHoraMinSegFormat(o.getHoraInicial()));
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file, true);
+            PrintWriter out = new PrintWriter(writer, true);
+            String str = "***********************************************************************";
+            out.println(str);
+            str += "[ ID ]:[" + o.getId() + "]";
+            out.println(str);
+            str += "[ Data Inicial ]:[" + Formatos.getDataHoraMinSegFormat(o.getHoraInicial());
+            out.println(str);
+            str += "[ Data Chegada Local ]:[" + Formatos.getDataHoraMinSegFormat(o.getHoraChegadaSocorro());
+            out.println(str);
+            str += "[ Data Saida Local ]:[" + Formatos.getDataHoraMinSegFormat(o.getHoraSaidaSocorro());
+            out.println(str);
+            str += "[ Data Chegada Hospital ]:[" + Formatos.getDataHoraMinSegFormat(o.getHoraChegadaHospital());
+            out.println(str);
+            str += "[ Data Saida Hospital ]:[" + Formatos.getDataHoraMinSegFormat(o.getHoraFinal());
+            out.println(str);
+            str += "[ Local Inicial ]:[ Lat:" + o.getLatLocalInicial() + " Long:"+ o.getLonLocalInicial()+ "]";
+            out.println(str);
+            str += "[ Local Local ]:[ Lat:" + o.getLatLocalChegadaSocorro() + " Long:"+ o.getLonLocalChegadaSocorro()+ "]";
+            out.println(str);
+            str += "[ Local Hospital ]:[Lat:" + o.getLatHospitalDestino() + " Long:"+ o.getLonHospitalDestino()+ "]";
+            out.println(str);
+            str += "[ Distancia Socorro ]:[" + o.getDistanciaSocorro() + "]";
+            out.println(str);
+            str += "[ Distancia Hospital ]:[" + o.getDistanciaHospital() + "]";
+            out.println(str);
+            str = "***********************************************************************";
+            out.println(str);
+            str = "***********************************************************************";
+            out.println(str);
+            for (bd_Trajecto p: t) {
+                str = "[" + p.getId() + "]:[" + Formatos.getDataHoraMinSegFormat(p.getData()) + "]:[" + p.getLatitude() + "]:[" + p.getLongitude() + "]";
+                out.println(str);
+
+            }
+            str = "***********************************************************************";
+            out.println(str);
+            out.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/td1026/gpsemergencia/BaseDados/bd_Trajecto.java
@@ -0,0 +1,82 @@
+package com.td1026.gpsemergencia.BaseDados;
+
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+import com.td1026.gpsemergencia.Dados.Posicao;
+
+import java.util.Date;
+
+/**
+ * Created by Telmo on 18-04-2016.
+ */
+
+@DatabaseTable(tableName = "bd_Trajecto")
+public class bd_Trajecto {
+
+    @DatabaseField(generatedId = true)
+    private Long id;
+    @DatabaseField
+    private Long id_ocorrencia;
+    @DatabaseField
+    private Date Data;
+    @DatabaseField
+    private Double Latitude;
+    @DatabaseField
+    private Double Longitude;
+
+    public bd_Trajecto() {
+    }
+    public bd_Trajecto(Long id , Posicao p) {
+        this.id_ocorrencia = id;
+        Data = p.getData();
+        Latitude = p.getLocal().getLatitude();
+        Longitude = p.getLocal().getLongitude();
+    }
+    public bd_Trajecto(Long id_ocorrencia, Date data, Double latitude, Double longitude) {
+        this.id_ocorrencia = id_ocorrencia;
+        Data = data;
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId_ocorrencia() {
+        return id_ocorrencia;
+    }
+
+    public void setId_ocorrencia(Long id_ocorrencia) {
+        this.id_ocorrencia = id_ocorrencia;
+    }
+
+    public Date getData() {
+        return Data;
+    }
+
+    public void setData(Date data) {
+        Data = data;
+    }
+
+    public Double getLatitude() {
+        return Latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        Latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return Longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        Longitude = longitude;
+    }
+}
